e5407ab1d645697bafb8a2e84675b762cd76825d
Jacob Fliss <newgmobile@gmail.com>
2020-04-22 13:01:16 -0400

    new pwa worker test

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,20 +1,18 @@
-// This is the service worker with the Cache-first network
+// This is the "Offline copy of assets" service worker
 
-const CACHE = "pwabuilder-precache";
+const CACHE = "pwabuilder-offline";
 
-importScripts(
-	"https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js"
-);
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
 self.addEventListener("message", (event) => {
-	if (event.data && event.data.type === "SKIP_WAITING") {
-		self.skipWaiting();
-	}
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
 workbox.routing.registerRoute(
-	new RegExp("/*"),
-	new workbox.strategies.CacheFirst({
-		cacheName: CACHE,
-	})
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
 );
