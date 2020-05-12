399d2d626315b42f5ad1da11a64ec5ecf49d88bf
Jacob Fliss <newgmobile@gmail.com>
2020-05-12 09:01:17 -0400

    forgot sw, also changed to network only

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,18 +1,3 @@
-// This is the "Offline copy of assets" service worker
-
-const CACHE = "pwabuilder-offline";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+self.addEventListener('fetch', function (event) {
+  event.respondWith(fetch(event.request));
 });
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
